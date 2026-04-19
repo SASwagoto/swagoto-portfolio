90458e264dc646fd1d52a8ec74f884992c43e8c6
SASwagoto <saswagoto@gmail.com>
2026-04-19 18:04:59 +0600

    added bgm

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { Routes, Route } from 'react-router-dom'
-import Layout from './components/Layout'
-import Home from './pages/Home'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout";
+import Home from "./pages/Home";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const audioRef = useRef(null);
+
+  useEffect(() => {
+    if (audioRef.current) {
+      audioRef.current.volume = 0.15;
+
+      const playAudio = () => {
+        audioRef.current.play().catch(() => {
+          console.log("Autoplay blocked. Waiting for user interaction...");
+        });
+      };
+      playAudio();
+      window.addEventListener("click", playAudio, { once: true });
+    }
+  }, []);
 
   return (
     <>
+      <audio ref={audioRef} loop>
+        <source src="/assets/audio/bgm.mp3" type="audio/mpeg" />
+      </audio>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
         </Route>
       </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
